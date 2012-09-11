d930ad2ce0fb64d7aa894eec5a3d7885d76f409d
yoshimuraYuu <yoshiyuu0310@gmail.com>
2012-09-11 13:55:04 +0900

    finish 09-04

--- s/make.pl
+++ s/make.pl
@@ -1,6 +1,10 @@
 use strict;
 use warnings;
 
+my @pwd = split '/', `pwd`;
+my $pwd = pop @pwd;
+chomp $pwd;
+
 my $length = shift @ARGV;
 
 my @mfile;
@@ -12,38 +16,39 @@ push @out, (shift @ARGV) for 0..$length-1;
 my @article;
 push @article, (shift @ARGV) for 0..$length-1;
 
-open my $fh, '<', '../tmpl/head.tex' or die "tmpl/head.tex is NOT exist!\n";
-my @lines = <$fh>;
-close $fh;
+# open my $fh, '<', '../../tmpl/head.tex' or die "tmpl/head.tex is NOT exist!\n";
+# my @lines = <$fh>;
+# close $fh;
 
-open $fh, '<', '../tmpl/section.tex' or die "tmpl/section.tex is NOT exist!\n";
+open my $fh, '<', '../../tmpl/section.tex' or die "tmpl/section.tex is NOT exist!\n";
 my $section = '';
 $section .= $_ while <$fh>;
 close $fh;
 
+my @lines;
 for (0..$length-1) {
 	my $newsec  = $section;
 	
-	$newsec =~ s/---MFILE---/$mfile[$_]/g;
+	$newsec =~ s/---MFILE---/\\lstinputlisting[style=matlab]{$pwd\/$mfile[$_]}/g;
 
 	my $result = '';
 	if ($out[$_]) {
-		$result = "\\lstinputlisting{$out[$_]}";
+		$result = "\\lstinputlisting{$pwd/$out[$_]}";
 	}
 	$newsec =~ s/---RESULTFILE---/$result/g;
 
 	my $input = '';
 	if ($article[$_]) {
-		$input = "\\input{$article[$_]}\n";
+		$input = "\\input{$pwd/$article[$_]}\n";
 	}
 	$newsec =~ s/---ARTICLEFILE---/$input/g;
 
 	push @lines, $newsec;
 }
 
-open $fh, '<', '../tmpl/end.tex' or die "tmpl/end.tex is NOT exist!\n";
-push @lines, $_ while <$fh>;
-close $fh;
+# open $fh, '<', '../../tmpl/end.tex' or die "tmpl/end.tex is NOT exist!\n";
+# push @lines, $_ while <$fh>;
+# close $fh;
 
 open $fh, '>', 'out.tex';
 print $fh $_ for @lines;
